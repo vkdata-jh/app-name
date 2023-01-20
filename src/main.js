b53e0162997f52ab6b9e4ca84c442da1f84ad961
vkdata-jh <info@vkdata.cz>
2023-01-20 12:46:19 +0100

    begining

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'devextreme/dist/css/dx.common.css';
+import './themes/generated/theme.base.css';
+import './themes/generated/theme.additional.css';
+import { createApp }  from "vue";
+import router from "./router";
+import themes from "devextreme/ui/themes";
 
-createApp(App).mount('#app')
+import App from "./App";
+import appInfo from "./app-info";
+
+themes.initialized(() => {
+    const app = createApp(App);
+    app.use(router);
+    app.config.globalProperties.$appInfo = appInfo;
+    app.mount('#app');
+});
